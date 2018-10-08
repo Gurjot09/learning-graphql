33102101ce79381979d2f12e737f9f5ef786435b
gurjot@maropost.com <gurjot@maropost.com>
2018-10-08 12:22:07 +0000

    creating a movie

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,10 +1,15 @@
-module Types
-  class MutationType < Types::BaseObject
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
-    end
+Types::MutationType = GraphQL::ObjectType.define do
+  name "Mutation"
+
+  field :createMovie, Types::MovieType do
+    argument :title, !types.String
+    argument :description, !types.String
+
+    resolve -> (obj, args, ctx) {
+      Movie.create(
+        title: args[:title],
+        description: args[:description]
+      )
+    }
   end
 end
